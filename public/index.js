be6d22f5741d4145fe93d3da7f804467be058a53
kassrojas <kassandrarojass@gmail.com>
2023-03-17 00:03:43 -0400

    successfully displaying information after btn click

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const termEl = document.querySelector('#terms');
+const categoryEl = document.querySelector('#categories');
 const btnFetchTerms = document.querySelector('#fetch-term-btn');
 const btnFetchCategories = document.querySelector('#fetch-cat-btn');
 
@@ -9,7 +10,7 @@ const getTerms = async () => {
   const json = await result.json();
   console.log(json);
   return json;
-}
+};
 
 const getCategories = async () => {
   const categories = await fetch('/api/categories', {
@@ -18,18 +19,38 @@ const getCategories = async () => {
   const jsonCat = await categories.json();
   console.log(jsonCat)
   return jsonCat;
-}
+};
 
+const renderTerms = (vocab) => {
+  termEl.innerHTML += `
+  <div class="card">
+  <h5 class="card-header">${vocab.category}</h5>
+  <div class="card-body">
+    <h5 class="card-title">${vocab.tech}</h5>
+    <p class="card-text">${vocab.definition}</p>
+    <a href="${vocab.url}" class="btn btn-primary" target="_blank">Look Further</a>
+  </div>
+</div>
+  `
+};
+
+const renderCategories = (vocab) => {
+  categoryEl.innerHTML += `
+  <div class="card"> 
+  <h5 class="card-title">${vocab}</h5>
+  </div>
+  `
+}
 
 const termBtnHandler = () => {
   console.log('btn');
-  getTerms();
+  getTerms().then((res) => res.forEach((item) => renderTerms(item)));
 };
 
 const catBtnHandler = () => {
   console.log('cat btn');
-  getCategories();
-}
+  getCategories().then((res) => res.forEach((item) => renderCategories(item)));
+};
 
 btnFetchTerms.addEventListener('click', termBtnHandler);
 btnFetchCategories.addEventListener('click', catBtnHandler);
